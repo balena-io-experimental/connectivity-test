f12adca18e8b5080c92fa5c1f41c2b770f2ab7c0
Gergely Imreh <gergely@balena.io>
2019-02-11 15:45:34 +0000

    Add `IDLE` env var to to stop the device from running any test just start up.
    Change-type: minor
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- testrun.sh
+++ testrun.sh
@@ -3,6 +3,13 @@
 # Start Avahi
 /usr/sbin/avahi-daemon -s &
 
-node src/index.js
-echo "Error code: $?"
-while : ; do sleep 600; done
+if [ -n "IDLE" ]; then
+  while : ; do
+    echo "Not running tests, just idling..."
+    sleep 60;
+  done
+else
+  node src/index.js
+  echo "Error code: $?"
+  while : ; do sleep 600; done
+fi
